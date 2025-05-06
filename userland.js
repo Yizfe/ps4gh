c77496656837137eea9560a742e6cca00677c128
Yizfe <152971307+Yizfe@users.noreply.github.com>
2025-05-05 23:09:29 -0700

    Update userland.js

--- userland.js
+++ userland.js
@@ -419,9 +419,15 @@ if (attempts >= 10000) {
 }
 
 
-if (buffer == '926100000') {
+var code_addr = new int64(0x26100000, 0x00000009);
+var buffer = p.syscall("sys_mmap", code_addr, 0x300000, 7, 0x41000, -1, 0);
+
+if (buffer.low === 0x26100000 && buffer.hi === 0x00000009) {
   writeGoldHEN(p, code_addr);
+} else {
+  alert("❌ sys_mmap failed, buffer: " + buffer.toString());
 }
+
 
 
 
